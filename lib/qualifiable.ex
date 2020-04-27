33eb8a5c2901e41adf5961e527c3cb23e4782d58
Eli <kimelijah90@gmail.com>
2020-04-27 18:46:13 -0400

    Implement Qualifiable for BackstageItem

--- lib/qualifiable.ex
+++ lib/qualifiable.ex
@@ -23,6 +23,24 @@ defimpl Qualifiable, for: GildedRose.AgingItem do
   end
 end
 
+defimpl Qualifiable, for: GildedRose.BackstageItem do
+  def update_quality(%{item: %{sell_in: 0} = item}) do
+    struct(@for, item: %{item | quality: 0})
+  end
+
+  def update_quality(%{item: %{sell_in: sell_in, quality: quality} = item}) when sell_in <= 5 do
+    struct(@for, item: %{item | sell_in: sell_in - 1, quality: quality + 3})
+  end
+
+  def update_quality(%{item: %{sell_in: sell_in, quality: quality} = item}) when sell_in <= 10 do
+    struct(@for, item: %{item | sell_in: sell_in - 1, quality: quality + 2})
+  end
+
+  def update_quality(%{item: %{sell_in: sell_in, quality: quality} = item}) do
+    struct(@for, item: %{item | sell_in: sell_in - 1, quality: quality + 1})
+  end
+end
+
 defimpl Qualifiable, for: Any do
   def update_quality(%struct{item: item}) do
     item =
