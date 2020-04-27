8ef20288f22d1f4351937a0e66f8bbd81fc2b02e
Eli <kimelijah90@gmail.com>
2020-04-27 19:18:47 -0400

    Refactory with min/max

--- lib/qualifiable.ex
+++ lib/qualifiable.ex
@@ -10,26 +10,18 @@ defimpl Qualifiable, for: GildedRose.LegendaryItem do
 end
 
 defimpl Qualifiable, for: GildedRose.AgingItem do
-  def update_quality(%{item: %{sell_in: 0}} = item) do
-    item
-  end
-
   def update_quality(%{item: %{sell_in: sell_in, quality: 50} = item}) do
     struct(@for, item: %{item | sell_in: sell_in - 1})
   end
 
   def update_quality(%{item: %{sell_in: sell_in, quality: quality} = item}) do
-    struct(@for, item: %{item | sell_in: sell_in - 1, quality: quality + 1})
+    struct(@for, item: %{item | sell_in: max(sell_in - 1, 0), quality: quality + 1})
   end
 end
 
 defimpl Qualifiable, for: GildedRose.BackstageItem do
   def update_quality(%{item: %{sell_in: 0} = item}) do
     struct(@for, item: %{item | quality: 0})
-  end
-
-  def update_quality(%{item: %{sell_in: sell_in, quality: quality} = item}) when quality > 50 do
-    struct(@for, item: %{item | sell_in: sell_in - 1})
   end
 
   def update_quality(%{item: %{sell_in: sell_in, quality: quality} = item}) when sell_in <= 5 do
@@ -41,25 +33,17 @@ defimpl Qualifiable, for: GildedRose.BackstageItem do
   end
 
   def update_quality(%{item: %{sell_in: sell_in, quality: quality} = item}) do
-    struct(@for, item: %{item | sell_in: sell_in - 1, quality: quality + 1})
+    struct(@for, item: %{item | sell_in: sell_in - 1, quality: min(quality + 1, 50)})
   end
 end
 
 defimpl Qualifiable, for: GildedRose.BaseItem do
-  def update_quality(%{item: %{sell_in: sell_in, quality: 0} = item}) do
-    struct(@for, item: %{item | sell_in: sell_in - 1, quality: 0})
-  end
-
-  def update_quality(%{item: %{sell_in: 0, quality: quality} = item}) when quality <= 2 do
-    struct(@for, item: %{item | sell_in: 0, quality: 0})
-  end
-
   def update_quality(%{item: %{sell_in: 0, quality: quality} = item}) do
-    struct(@for, item: %{item | sell_in: 0, quality: quality - 2})
+    struct(@for, item: %{item | sell_in: 0, quality: max(quality - 2, 0)})
   end
 
   def update_quality(%{item: %{sell_in: sell_in, quality: quality} = item}) do
-    struct(@for, item: %{item | sell_in: sell_in - 1, quality: quality - 1})
+    struct(@for, item: %{item | sell_in: sell_in - 1, quality: max(quality - 1, 0)})
   end
 end
 
